6e7633646ef8f98ad477301903863ff681abd940
iBitHub <42367775+ibithub@users.noreply.github.com>
2021-02-25 20:25:28 -0700

    Update main.h

--- src/main.h
+++ src/main.h
@@ -1,5 +1,6 @@
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Copyright (c) 2018-2020 iBitHub Developers
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers	// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2018-2021 iBitHub Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_MAIN_H
