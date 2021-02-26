08c69b81ba2d4cf25674df5efddfacc71c6499a6
iBitHub <42367775+ibithub@users.noreply.github.com>
2021-02-25 20:27:11 -0700

    Update addrman.cpp

--- src/addrman.cpp
+++ src/addrman.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2012 Pieter Wuille
 // Copyright (c) 2009-2018 The Bitcoin Core developers
-// Copyright (c) 2018-2020 iBitHub Developers
+// Copyright (c) 2018-2021 iBitHub Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
